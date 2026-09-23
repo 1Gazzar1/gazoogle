7701f5ab04c5b922dc33a7d7fa257a5e44e2c12d
1Gazzar1 <mhmdaljzar036@gmail.com>
2026-09-23 22:39:57 +0300

    fix: update service restart policies and correct SQL query conditions

--- query_engine/src/db/queries/links.sql
+++ query_engine/src/db/queries/links.sql
@@ -1,17 +1,17 @@
 -- name: GetBacklinkCount :many
 SELECT to_page_id,COUNT(*) AS backlink_count
 FROM links 
-WHERE to_page_id = ANY(@link_ids::int[])
+WHERE to_page_id = ANY(@link_ids::int[]) AND to_page_id != from_page_id -- correction for pages that link to them selves (most of them lol)
 GROUP BY to_page_id;
 
 -- name: GetForwardlinks :many
 SELECT from_page_id,to_page_id,url,title 
 FROM links 
 JOIN pages ON pages.id = to_page_id 
-WHERE from_page_id = @page_id::int;
+WHERE from_page_id = @page_id::int  AND to_page_id != from_page_id; 
 
 -- name: GetBacklinks :many
 SELECT from_page_id,to_page_id,url,title 
 FROM links 
 JOIN pages ON pages.id = from_page_id 
-WHERE to_page_id = @page_id::int;
+WHERE to_page_id = @page_id::int AND to_page_id != from_page_id; 
